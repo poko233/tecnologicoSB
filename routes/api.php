61baaff8ed9af8ed2ebb31729c8fd0c79efdb8c4
aron <aronpad123@gmail.com>
2026-05-11 18:03:33 -0400

    feat: add crud modulos

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,11 @@
 use App\Http\Controllers\RolController;
 
 Route::apiResource('roles', RolController::class);
+Route::apiResource('modulos', ModuloController::class);
+Route::prefix('modulos/{modulo}/formularios')->group(function () {
+    Route::get('/',          [FormularioModuloController::class, 'index']);
+    Route::post('/',         [FormularioModuloController::class, 'store']);
+    Route::put('/{formulario}',    [FormularioModuloController::class, 'update']);
+    Route::delete('/{formulario}', [FormularioModuloController::class, 'destroy']);
+    Route::post('/sync',     [FormularioModuloController::class, 'sync']);
+});
