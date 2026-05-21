eb3ab17ee113880fec0b9ff8333b8343491de105
López Cristian <lsc5001339@est.univalle.edu>
2026-05-21 17:23:52 -0400

    Todos los inscritos estan con rol Estudiante

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -11,7 +11,9 @@ class EstudianteController extends Controller
     public function index()
     {
         return response()->json([
-            'estudiantes' => User::latest('id')->get()
+            'estudiantes' => User::with(['numeroReferencias', 'roles'])
+                ->latest('id')
+                ->get()
         ]);
     }
 
@@ -21,12 +23,19 @@ class EstudianteController extends Controller
             'apellidoPaterno' => 'required|string|max:50',
             'apellidoMaterno' => 'required|string|max:50',
             'nombres' => 'required|string|max:50',
+
             'genero' => 'required|string|in:MASCULINO,FEMENINO',
+
             'carnet' => 'required|string|max:50|unique:user,ci',
+
             'expedidoEn' => 'required|string|in:LPZ,CBBA,OR,PT,TJ,SCZ,BN,PD,CH,QR,EXT',
+
             'fechaNacimiento' => 'required|date',
+
             'direccion' => 'required|string|max:50',
+
             'celular' => 'required|string|max:20',
+
             'referenciaNombre' => 'required|string|max:50',
             'referenciaParentesco' => 'required|string|max:50',
             'referenciaNumero' => 'required|string|max:50',
@@ -53,6 +62,15 @@ class EstudianteController extends Controller
             'verificacion' => 0,
         ]);
 
+        // =========================
+        // ASIGNAR ROL ESTUDIANTE
+        // =========================
+        // Rol Estudiante = ID 2
+        $estudiante->roles()->syncWithoutDetaching([2]);
+
+        // =========================
+        // GUARDAR REFERENCIA
+        // =========================
         $estudiante->numeroReferencias()->create([
             'nombreContactoReferencia' => $validated['referenciaNombre'],
             'parentesco' => $validated['referenciaParentesco'],
@@ -61,13 +79,19 @@ class EstudianteController extends Controller
 
         return response()->json([
             'message' => 'Estudiante registrado correctamente',
-            'estudiante' => $estudiante->load('numeroReferencias')
+            'estudiante' => $estudiante->load([
+                'numeroReferencias',
+                'roles'
+            ])
         ], 201);
     }
 
     public function show(string $id)
     {
-        $estudiante = User::with('numeroReferencias')->findOrFail($id);
+        $estudiante = User::with([
+            'numeroReferencias',
+            'roles'
+        ])->findOrFail($id);
 
         return response()->json([
             'estudiante' => $estudiante
@@ -82,12 +106,19 @@ class EstudianteController extends Controller
             'apellidoPaterno' => 'sometimes|required|string|max:50',
             'apellidoMaterno' => 'sometimes|required|string|max:50',
             'nombres' => 'sometimes|required|string|max:50',
+
             'genero' => 'sometimes|required|string|in:MASCULINO,FEMENINO',
+
             'carnet' => 'sometimes|required|string|max:50|unique:user,ci,' . $id . ',id',
+
             'expedidoEn' => 'sometimes|required|string|in:LPZ,CBBA,OR,PT,TJ,SCZ,BN,PD,CH,QR,EXT',
+
             'fechaNacimiento' => 'sometimes|required|date',
+
             'direccion' => 'sometimes|required|string|max:50',
+
             'celular' => 'sometimes|required|string|max:20',
+
             'referenciaNombre' => 'sometimes|required|string|max:50',
             'referenciaParentesco' => 'sometimes|required|string|max:50',
             'referenciaNumero' => 'sometimes|required|string|max:50',
@@ -151,7 +182,10 @@ class EstudianteController extends Controller
 
         return response()->json([
             'message' => 'Estudiante actualizado',
-            'estudiante' => $estudiante->load('numeroReferencias')
+            'estudiante' => $estudiante->load([
+                'numeroReferencias',
+                'roles'
+            ])
         ]);
     }
 
@@ -160,6 +194,10 @@ class EstudianteController extends Controller
         $estudiante = User::findOrFail($id);
 
         $estudiante->numeroReferencias()->delete();
+
+        // Eliminar relaciones de roles
+        $estudiante->roles()->detach();
+
         $estudiante->delete();
 
         return response()->json([
